f353bc4f28c2414cdaf329ee9eed5c020c073231
Tambe Barsbay <tambeb@gmail.com>
2019-06-11 21:13:07 -0400

    formatting

--- server_logger.js
+++ server_logger.js
@@ -15,74 +15,74 @@ var udpPortServerWeb = serverSettings.udpPortServerWeb;
 var udpPortShake = serverSettings.udpPortShake;
 
 function sendUpdateToServerWeb( whichStream, msg ) {
-  let data = JSON.stringify( { whichStream: whichStream, message: msg } );
-  serverUdpWeb.send( data, udpPortServerWeb, ( err ) => {
-  } );
+    let data = JSON.stringify( { whichStream: whichStream, message: msg } );
+    serverUdpWeb.send( data, udpPortServerWeb, ( err ) => {
+    } );
 
 }
 
 serverUdpShake.on( 'error', ( err ) => {
-  serverUdpShake.close();
+    serverUdpShake.close();
 } );
 
 serverUdpShake.on( 'listening', () => {
 } );
 
 serverUdpShake.on( 'message', ( msg, rinfo ) => {
-  msg = `${ msg }`;
-  msg = removeBraces( msg );
-  msg = removeSingleQuotes( msg );
-  msg = msg.trimStart().split( ',' );
-  let whichStream = whichDataStream( msg );
-  rawAppend( msg );
-  sendUpdateToServerWeb( whichStream, msg );
+    msg = `${ msg }`;
+    msg = removeBraces( msg );
+    msg = removeSingleQuotes( msg );
+    msg = msg.trimStart().split( ',' );
+    let whichStream = whichDataStream( msg );
+    rawAppend( msg );
+    sendUpdateToServerWeb( whichStream, msg );
 } );
 
 serverUdpShake.bind( udpPortShake );
 
 function rawAppend( msg ) {
-  let dataType = msg[ 0 ];
-  msg = removeDataType( msg );
-  msg[ 0 ] = fixEpoch( msg[ 0 ] );
-  let file = getFilenameDate( msg[ 0 ] ) + '_' + dataType + '.log';
-  msg[ 0 ] = reformatEpoch( msg[ 0 ] );
-  appendNewLine( msg, file );
+    let dataType = msg[ 0 ];
+    msg = removeDataType( msg );
+    msg[ 0 ] = fixEpoch( msg[ 0 ] );
+    let file = getFilenameDate( msg[ 0 ] ) + '_' + dataType + '.log';
+    msg[ 0 ] = reformatEpoch( msg[ 0 ] );
+    appendNewLine( msg, file );
 }
 function whichDataStream( msg ) {
-  return msg[ 0 ];
+    return msg[ 0 ];
 }
 function openJsonSync( fileName ) {
-  let file = path.join( root, fileName );
-  fs.ensureFileSync( file );
-  return fs.readJsonSync( file );
+    let file = path.join( root, fileName );
+    fs.ensureFileSync( file );
+    return fs.readJsonSync( file );
 }
 function removeBraces( data ) {
-  return data.replace( /[{}]/g, '' );
+    return data.replace( /[{}]/g, '' );
 }
 function removeSingleQuotes( data ) {
-  return data.replace( /'/g, '' );
+    return data.replace( /'/g, '' );
 }
 function removeDataType( data ) {
-  return data.slice( 1 );
+    return data.slice( 1 );
 }
 function fixEpoch( data ) {
-  return ( data * 1000 );
+    return ( data * 1000 );
 }
 function reformatEpoch( data ) {
-  return format( parse( data ), dateFormat );
+    return format( parse( data ), dateFormat );
 }
 function getFilenameDate( data ) {
-  return format( parse( data ), filenameDateFormat );
+    return format( parse( data ), filenameDateFormat );
 }
 function appendNewLine( data, filename ) {
-  let file = path.join( root, 'logs', filename );
-  fs.ensureFile( file ).then(
-    () => {
-      fs.appendFile( file, data + '\n', function ( err ) { } );
-    }
-  )
+    let file = path.join( root, 'logs', filename );
+    fs.ensureFile( file ).then(
+        () => {
+            fs.appendFile( file, data + '\n', function ( err ) { } );
+        }
+    )
 }
 process.on( 'uncaughtException', function ( err ) {
-  var message = err.message;
-  console.log( err );
+    var message = err.message;
+    console.log( err );
 } )
